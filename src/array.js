f27fb4ff252c259dbe60fa82057d64b5bf686b77
xuexing <xuexing9527@163.com>
2019-11-13 20:15:48 +0800

    数组 取 交集 初版

--- src/array.js
+++ src/array.js
@@ -1,6 +1,31 @@
 'use strict';
 const sweetArray = function() {};
 
-// sweetArray
+// 数组 取并集
+sweetArray.prototype.intersect = function (arrays, key) {
+  console.log(arrays);
+  let arr = [];
+  Array.prototype.forEach.call(arrays, function (item) {
+    if (Object.prototype.toString.call(item) === '[object Array]') {
+      arr = arr.concat(item);
+    } else {
 
-// 两个数组 取交集
+    }
+  });
+  const map = new Map();
+  if (key) {
+    arr.forEach(function (item, index) {
+      if (Object.prototype.toString.call(item) === '[object Object]') {
+        map.set(item[key], item);
+      } else {
+        throw Error('参数 key 存在时 ，子元素非 Object，目前仅支持子元素 同为 Object 类型！');
+      }
+    });
+    return [ ...map.values() ];
+  } else {
+    arr.forEach(function (item, index) {
+      map.set(item, index);
+    });
+    return [ ...map.keys() ];
+  }
+}
