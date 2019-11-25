8f2e41ea46d28f99dd0bf7596aea721d6d641c9e
xuexing <xuexing9527@163.com>
2019-11-25 18:53:45 +0800

    update

--- src/array.js
+++ src/array.js
@@ -1,8 +1,9 @@
 'use strict';
 const sweetArray = function() {};
 
-// 数组 取并集
+// 数组 根据存在key的数组元素 去重(后元素取代前元素) 合并
 sweetArray.prototype.intersect = function (arrays, key) {
+  const start = new Date().getTime();
   console.log(arrays);
   const num = 5;
   let arr = [];
@@ -13,77 +14,39 @@ sweetArray.prototype.intersect = function (arrays, key) {
 
     }
   });
+  console.log(arr);
   const map = new Map();
   if (key) {
-    arr.forEach(function (item) {
+    for (let arrKey in arr) {
+      const item = arr[arrKey];
+      console.log(1);
       if (Object.prototype.toString.call(item) === '[object Object]') {
         if (item[key]) {
           map.set(item[key], item);
         } else {
-
-          // const map = new Map();
-          // const item = { a: { b: { c: { d: 1} } } };
-          // key = 'd';
-
-
-          // 循环
-          // let hasVaule = false;
-          let loop = true;
-          let noneKey = true;
-          let arr = [];
-          const start = new Date().getTime();
-
-          let loopNum = 0;
-          // const loopKeyArr = [];
-          while (new Date().getTime() - start < 3000) {
-            console.log(arr.length)
-            console.log(new Date().getTime() - start < 3000);
-
-
-            for (let n = 0; n < loopNum; n += 1) {
-
-            }
-
-            let arrNext = [];
-            for (let k in item) {
-              if (item[k]) {
-                arrNext.push(item[k])
-              }
-            }
-            arr = arrNext;
-
-
-            a: for (let i = 0; i < arr.length; i += 1) {
-              for (let k in arr[i]) {
-                if (arr[i][key]) {
-                  map.set(arr[i][key], item);
-                  loop = false;
-                  noneKey = false;
-                  break a;
+          const r = function (obj) {
+            console.log('我进了递归...')
+            if (obj[key]) {
+              map.set(obj[key], item);
+            } else {
+              for (let k in obj) {
+                if (obj[k]) {
+                  return r(obj[k]);
                 }
               }
             }
-
-            console.log(arrNext);
-            loopNum += 1;
-            console.log(loopNum);
-            // if (noneKey) {
-            //   let arrNext = [];
-            //   for (let k in item) {
-            //     if (item[k]) {
-            //       arrNext.push(item[k])
-            //     }
-            //   }
-            //   arr = arrNext;
-            //   console.log(arrNext);
-            // }
           }
-
+          for (let k in item) {
+            if (item[k]) {
+              r(item[k]);
+            }
+          }
         }
       } else {
         throw Error('参数 key 存在时 ，子元素非 Object，目前仅支持子元素 同为 Object 类型！');
       }
-    });
+    }
+    console.log(`${new Date().getTime() - start} ms 完成`)
     return [ ...map.values() ];
   } else {
     arr.forEach(function (item, index) {
@@ -91,4 +54,24 @@ sweetArray.prototype.intersect = function (arrays, key) {
     });
     return [ ...map.keys() ];
   }
+}
+
+
+const l = function (num) {
+  const start = new Date().getTime();
+  const f = function (n1, n2, n) {
+    if (n > 0) {
+      if (!n) {
+        console.log(`用时${new Date().getTime() - start} ms`);
+        return;
+      }
+      const value = n1 + n2;
+      console.log(value)
+      if (n == 1 || n == 2) {
+        console.log(1);
+      }
+      return f(n2, value, n - 1);
+    }
+  }
+  f(1, 1, num)
 }
